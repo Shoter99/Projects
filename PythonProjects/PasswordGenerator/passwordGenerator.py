743f0a3176c8a4e587b0dd6b7cfd830df2b5594b
Shoter99 <shoter998@gmail.com>
2020-08-02 21:22:07 +0200

    Pyton Project

---
+++ PythonProjects/PasswordGenerator/passwordGenerator.py
@@ -0,0 +1,36 @@
+import random
+
+numbers = ['0','1','2','3','4','5','6','7','8','9']
+smallLetters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','p','o','q','r','s','t','u','w','y','z']
+bigLetters = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','W','Y','Z']
+characters = ['!','@','#','$','%','&','*']
+password = []
+i = 15
+
+while(i >= 0):
+    rn = random.randint(0,9)
+    rs = random.randint(0,23)
+    rb = random.randint(0,23)
+    rc = random.randint(0,6)
+    r = random.randint(0,12)
+    if(r == 0 or r == 4 or r == 8 or r == 12):
+        password.append(numbers[rn])
+    if(r == 1 or r == 5 or r == 9):
+        password.append(smallLetters[rs])
+    elif(r == 2 or r == 6 or r == 10):
+        password.append(bigLetters[rb])
+    elif(r == 3 or r == 7 or r == 11):
+        password.append(characters[rc])
+    i-=1
+
+
+def convert_list_to_string(org_list, separator = ''):
+    return separator.join(org_list)
+
+full_password = convert_list_to_string(password, '')
+
+whatfor = input("Do czego jest to haslo? ")
+f = open("passwords.txt", "a")
+f.write(whatfor+": "+full_password+"\n")
+f.close()
+print("Twoje haslo: "+full_password)
---
+++ PythonProjects/hangman.py
@@ -0,0 +1,7 @@
+import random as rand
+import time
+
+x = rand.randint(0,3)
+
+hasla = ["miasto", "rekin", "komputer", "kawiatura"]
+print(hasla[x])
