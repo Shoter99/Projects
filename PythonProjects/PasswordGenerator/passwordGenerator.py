ba0698f85a7f2727fda18c3222984c4c303bb22c
Shoter99 <shoter998@gmail.com>
2020-08-03 08:10:12 +0200

    Update

--- PythonProjects/PasswordGenerator/passwordGenerator.py
+++ PythonProjects/PasswordGenerator/passwordGenerator.py
@@ -5,23 +5,43 @@ smallLetters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','p','o',
 bigLetters = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','W','Y','Z']
 characters = ['!','@','#','$','%','&','*']
 password = []
-i = 15
+i = int(input("Jak dlugie ma byc haslo? "))
 
-while(i >= 0):
-    rn = random.randint(0,9)
-    rs = random.randint(0,23)
-    rb = random.randint(0,23)
-    rc = random.randint(0,6)
-    r = random.randint(0,12)
-    if(r == 0 or r == 4 or r == 8 or r == 12):
-        password.append(numbers[rn])
-    if(r == 1 or r == 5 or r == 9):
-        password.append(smallLetters[rs])
-    elif(r == 2 or r == 6 or r == 10):
-        password.append(bigLetters[rb])
-    elif(r == 3 or r == 7 or r == 11):
-        password.append(characters[rc])
-    i-=1
+
+choice = input('Czy haslo ma miec znaki specjalne? Jesli tak to napisz "tak". Jesli haslo nie ma zawierac znakow specjalnych kliknij enter: ')
+if (choice == "tak" or choice == "Tak"):
+    while(i >= 0):
+        rn = random.randint(0,9)
+        rs = random.randint(0,23)
+        rb = random.randint(0,23)
+        rc = random.randint(0,6)
+        r = random.randint(0,12)
+        if(r == 0 or r == 4 or r == 8 or r == 12):
+            password.append(numbers[rn])
+        if(r == 1 or r == 5 or r == 9):
+            password.append(smallLetters[rs])
+        elif(r == 2 or r == 6 or r == 10):
+            password.append(bigLetters[rb])
+        elif(r == 3 or r == 7 or r == 11):
+            password.append(characters[rc])
+        i-=1
+else:
+    while(i >= 0):
+        rn = random.randint(0,9)
+        rs = random.randint(0,23)
+        rb = random.randint(0,23)
+        rc = random.randint(0,6)
+        r = random.randint(0,12)
+        if(r == 0 or r == 4 or r == 8 or r == 12):
+            password.append(numbers[rn])
+        if(r == 1 or r == 5 or r == 9):
+            password.append(smallLetters[rs])
+        elif(r == 2 or r == 6 or r == 10):
+            password.append(bigLetters[rb])
+        
+        i-=1
+
+
 
 
 def convert_list_to_string(org_list, separator = ''):
