c3ffcd964fabf2ce100e6f0b2a5311b1f2025aa7
Shoter99 <69056247+Shoter99@users.noreply.github.com>
2020-10-04 10:34:07 +0200

    C++

---
+++ C++/test/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+string imie;
+int main()
+{
+    cout << "Hello world!" << endl;
+    cout << "Podaj swoje imie: "<< endl;
+    cin>>imie;
+    cout<< "Witaj "+imie+"!";
+    return 0;
+}
---
+++ fractal.java
@@ -0,0 +1,13 @@
+float x = 0;
+float y = 0;
+
+void setup(){
+    size(600,600);
+}
+void draw(){
+    background(0);
+
+    stroke(255);
+    strokeWeight(8);
+    point(x,y)
+}
