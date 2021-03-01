6ce0d2311e0054c4eb83862056cf791a2c5fd560
Shoter99 <shoter998@gmail.com>
2021-03-01 22:51:03 +0100

    a

--- PythonProjects/IlandWebApp/app.py
+++ PythonProjects/IlandWebApp/app.py
@@ -1,13 +1,31 @@
 from flask import Flask, request, url_for, redirect, render_template
-
+from flask_mail import Mail, Message
+import os
 
 app = Flask(__name__)
 
+mail_settings = {
+    "MAIL_SERVER": 'smtp.gmail.com',
+    "MAIL_PORT": 465,
+    "MAIL_USE_TLS": False,
+    "MAIL_USE_SSL": True,
+    "MAIL_USERNAME": 'serveriland@gmail.com',
+    "MAIL_PASSWORD": 'i23092005'
+}
+app.config.update(mail_settings)
+mail = Mail(app)
 
-@app.route("/")
+@app.route("/", methods=["POST", "GET"])
 def home():
     return render_template("index.html")
 
 
+
 if __name__ == "__main__":
+    with app.app_context():
+        msg = Message(subject="test",
+            sender=app.config.get("MAIL_USERNAME"),
+            recipients=["shoter998@gmail.com"],
+            body="This is test")
+        mail.send(msg)
     app.run(debug=True)
